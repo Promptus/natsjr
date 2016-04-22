25829c53f18aa10316ae61d8fe1d54961add700e
Armin Pašalić <armin@pasalic.me>
2016-04-22 11:41:46 +0200

    Move specs to appropriate folder

--- natsjr.gemspec
+++ natsjr.gemspec
@@ -34,5 +34,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "pry", "~> 0.10"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency "simplecov"
   spec.add_development_dependency "codeclimate-test-reporter", "~> 0.5"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,18 @@
+require 'simplecov'
+require 'rspec'
+SimpleCov.coverage_dir("tmp/coverage")
+SimpleCov.start
+
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
+
+ENV["NATSJR_ENV"] = "test"
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 $PROGRAM_NAME = Dir.pwd + "/spec/spec_helper.rb"
-ENV["NATSJR_ENV"] = "test"
+
 require 'natsjr'
 
 def byte(str)
   NatsJr::JString.new(MultiJson.dump(str)).get_bytes
 end
-
